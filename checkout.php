ece6bd430c4249dc8d7ad0163dee72325cc49f4f
mohdshadadhossain <shadadhussain1@hotmail.com>
2023-05-10 22:23:34 +0100

    checkout page completed

--- checkout.php
+++ checkout.php
@@ -99,49 +99,49 @@ if(isset($_POST['order'])){
 
         <div class="flex">
             <div class="inputBox">
-                <span>your name :</span>
-                <input type="text" name="name" placeholder="enter your name">
+                <span>Your name :</span>
+                <input type="text" name="name" placeholder="Enter your name">
             </div>
             <div class="inputBox">
-                <span>your number :</span>
-                <input type="number" name="number" min="0" placeholder="enter your number">
+                <span>Your number :</span>
+                <input type="number" name="number" min="0" placeholder="Enter your number">
             </div>
             <div class="inputBox">
-                <span>your email :</span>
-                <input type="email" name="email" placeholder="enter your email">
+                <span>Your email :</span>
+                <input type="email" name="email" placeholder="Enter your email">
             </div>
             <div class="inputBox">
-                <span>payment method :</span>
+                <span>Payment method :</span>
                 <select name="method">
-                    <option value="cash on delivery">cash on delivery</option>
-                    <option value="credit card">credit card</option>
-                    <option value="paypal">paypal</option>
-                    <option value="paytm">paytm</option>
+                    <option value="cash on delivery">Cash on Delivery</option>
+                    <option value="credit card">Credit Card</option>
+                    <option value="paypal">PayPal</option>
+                    <option value="paytm">PayTm</option>
                 </select>
             </div>
             <div class="inputBox">
-                <span>address line 01 :</span>
+                <span>Address line 01 :</span>
                 <input type="text" name="flat" placeholder="e.g. flat no.">
             </div>
             <div class="inputBox">
-                <span>address line 02 :</span>
-                <input type="text" name="street" placeholder="e.g.  streen name">
+                <span>Address line 02 :</span>
+                <input type="text" name="Street" placeholder="e.g.  street name">
             </div>
             <div class="inputBox">
-                <span>city :</span>
-                <input type="text" name="city" placeholder="e.g. mumbai">
+                <span>City :</span>
+                <input type="text" name="City" placeholder="e.g. Derby">
             </div>
             <div class="inputBox">
                 <span>state :</span>
-                <input type="text" name="state" placeholder="e.g. maharashtra">
+                <input type="text" name="state" placeholder="e.g. Derbyshire">
             </div>
             <div class="inputBox">
-                <span>country :</span>
-                <input type="text" name="country" placeholder="e.g. india">
+                <span>Country :</span>
+                <input type="text" name="country" placeholder="e.g. United Kingdom">
             </div>
             <div class="inputBox">
-                <span>pin code :</span>
-                <input type="number" min="0" name="pin_code" placeholder="e.g. 123456">
+                <span>Postal code :</span>
+                <input type="number" min="0" name="pin_code" placeholder="e.g. DE23 8QJ">
             </div>
         </div>
 
