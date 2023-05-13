bde83b1f8f0716996c933902b6da001c53fc05f5
mohdshadadhossain <shadadhussain1@hotmail.com>
2023-05-13 14:44:04 +0100

    header drop down removed

--- header.php
+++ header.php
@@ -1,13 +1,13 @@
 <?php
-if(isset($message)){
-   foreach($message as $message){
-      echo '
+if (isset($message)) {
+    foreach ($message as $message) {
+        echo '
       <div class="message">
-         <span>'.$message.'</span>
+         <span>' . $message . '</span>
          <i class="fas fa-times" onclick="this.parentElement.remove();"></i>
       </div>
       ';
-   }
+    }
 }
 ?>
 
@@ -20,12 +20,8 @@ if(isset($message)){
         <nav class="navbar">
             <ul>
                 <li><a href="home.php">Home</a></li>
-                <li><a href="#">Pages </a>
-                    <ul>
-                        <li><a href="about.php">About</a></li>
-                        <li><a href="contact.php">Contact</a></li>
-                    </ul>
-                </li>
+                <li><a href="about.php">About</a></li>
+                <li><a href="contact.php">Contact</a></li>
                 <li><a href="shop.php">Flower & Gifts</a></li>
                 <li><a href="orders.php">Orders</a></li>
                 <li><a href="#">Account</a>
@@ -42,20 +38,28 @@ if(isset($message)){
             <a href="search_page.php" class="fas fa-search"></a>
             <div id="user-btn" class="fas fa-user"></div>
             <?php
-                $select_wishlist_count = mysqli_query($conn, "SELECT * FROM `wishlist` WHERE user_id = '$user_id'") or die('query failed');
-                $wishlist_num_rows = mysqli_num_rows($select_wishlist_count);
+            $select_wishlist_count = mysqli_query($conn, "SELECT * FROM `wishlist` WHERE user_id = '$user_id'") or die('query failed');
+            $wishlist_num_rows = mysqli_num_rows($select_wishlist_count);
             ?>
-            <a href="wishlist.php"><i class="fas fa-heart"></i><span>(<?php echo $wishlist_num_rows; ?>)</span></a>
+            <a href="wishlist.php"><i class="fas fa-heart"></i><span>(
+                    <?php echo $wishlist_num_rows; ?>)
+                </span></a>
             <?php
-                $select_cart_count = mysqli_query($conn, "SELECT * FROM `cart` WHERE user_id = '$user_id'") or die('query failed');
-                $cart_num_rows = mysqli_num_rows($select_cart_count);
+            $select_cart_count = mysqli_query($conn, "SELECT * FROM `cart` WHERE user_id = '$user_id'") or die('query failed');
+            $cart_num_rows = mysqli_num_rows($select_cart_count);
             ?>
-            <a href="cart.php"><i class="fas fa-shopping-cart"></i><span>(<?php echo $cart_num_rows; ?>)</span></a>
+            <a href="cart.php"><i class="fas fa-shopping-cart"></i><span>(
+                    <?php echo $cart_num_rows; ?>)
+                </span></a>
         </div>
 
         <div class="account-box">
-            <p>username : <span><?php echo $_SESSION['user_name']; ?></span></p>
-            <p>email : <span><?php echo $_SESSION['user_email']; ?></span></p>
+            <p>username : <span>
+                    <?php echo $_SESSION['user_name']; ?>
+                </span></p>
+            <p>email : <span>
+                    <?php echo $_SESSION['user_email']; ?>
+                </span></p>
             <a href="logout.php" class="delete-btn">logout</a>
         </div>
 
