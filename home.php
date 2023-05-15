427aafc17ce0dbc519719d92210f7873425afaf5
mohdshadadhossain <shadadhussain1@hotmail.com>
2023-05-16 00:54:38 +0100

    back to how it was

--- home.php
+++ home.php
@@ -78,8 +78,7 @@ if(isset($_POST['add_to_cart'])){
 <body>
    
 <?php @include 'header.php'; ?>
-
-<!-- <section class="home">
+<section class="home">
 
    <div class="content">
       <h3>Luxury Flowers</h3>
@@ -87,79 +86,13 @@ if(isset($_POST['add_to_cart'])){
       <a href="about.php" class="btn">discover more</a>
    </div>
 
-</section> -->
-<!-- Container for the image gallery -->
-<div class="container">
-
-  <!-- Full-width images with number text -->
-  <div class="mySlides">
-    <div class="numbertext">1 / 6</div>
-      <img src="images/bg-flower.jpg" width="1390" height="400" >
-  </div>
-
-  <div class="mySlides">
-    <div class="numbertext">2 / 6</div>
-      <img src="images/slider-1.jpg" width="1390" height="400">
-  </div>
-
-  <div class="mySlides">
-    <div class="numbertext">3 / 6</div>
-      <img src="images/slider-2.jpg" width="1390" height="400">
-  </div>
-
-  <div class="mySlides">
-    <div class="numbertext">4 / 6</div>
-      <img src="images/slider-3.jpg" width="1390" height="400">
-  </div>
-
-  <div class="mySlides">
-    <div class="numbertext">5 / 6</div>
-      <img src="images/slider-5.jpg" width="1390" height="400">
-  </div>
-
-  <div class="mySlides">
-    <div class="numbertext">6 / 6</div>
-      <img src="images/slider-6.jpg"  width="1390" height="400">
-  </div>
-
-  <!-- Next and previous buttons -->
-  <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
-  <a class="next" onclick="plusSlides(1)">&#10095;</a>
-
-  <!-- Image text -->
-  <div class="caption-container">
-    <p id="caption"></p>
-  </div>
-
-  <!-- Thumbnail images -->
-  <div class="row">
-    <div class="column">
-      <img class="demo cursor" src="images/bg-flower.jpg" width="100" height="80" style="width:100%" onclick="currentSlide(1)" alt="The Woods">
-    </div>
-    <div class="column">
-      <img class="demo cursor" src="images/slider-1.jpg" width="100" height="80" style="width:100%" onclick="currentSlide(2)" alt="Cinque Terre">
-    </div>
-    <div class="column">
-      <img class="demo cursor" src="images/slider-2.jpg" width="100" height="80" style="width:100%" onclick="currentSlide(3)" alt="Mountains and fjords">
-    </div>
-    <div class="column">
-      <img class="demo cursor" src="images/slider-3.jpg" width="100" height="80" style="width:100%" onclick="currentSlide(4)" alt="Northern Lights">
-    </div>
-    <div class="column">
-      <img class="demo cursor" src="images/slider-5.jpg" width="100" height="80" style="width:100%" onclick="currentSlide(5)" alt="Nature and sunrise">
-    </div>
-    <div class="column">
-      <img class="demo cursor" src="images/slider-6.jpg" width="100" height="80" style="width:100%" onclick="currentSlide(6)" alt="Snowy Mountains">
-    </div>
-  </div>
-</div>
+</section>
 
 <section class="products">
 
    <h1 class="title">latest products</h1>
 
    <div class="box-container">
-
       <?php
          $select_products = mysqli_query($conn, "SELECT * FROM `products` LIMIT 6") or die('query failed');
          if(mysqli_num_rows($select_products) > 0){
